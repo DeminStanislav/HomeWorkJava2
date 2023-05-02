cee9fbfbc9cc7ab4947fad1e8235125caffa96af
Stanislav <deminstanislav1988@mail.ru>
2023-05-02 12:16:01 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 25000;
+        int bonus = amount / 20;
+                System.out.println("Начисленные баллы: " + bonus);
+
+      }
+    }
